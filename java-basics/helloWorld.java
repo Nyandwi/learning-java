c52128d09efb1ef080b77c3e12f537cbea454eb4
Nyandwi <johnjw7084@gmail.com>
2021-10-06 18:31:32 +0200

    Added hello world

---
+++ java-basics/helloWorld.java
@@ -0,0 +1,42 @@
+
+// Hello world is a simple program that just display Hello World message
+// This is how you add comment in Java
+/* You can also add comment like this */
+
+// HELLO WORLD
+
+class helloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
+}
+
+// SOME NOTES
+
+// This is comment. Comments are ignored by Java compiler
+// In Java, every program starts with the class definition
+
+// HelloWorld is the name of the class and below is how the class is defined
+// The class name must be similar to the filename
+//class helloWorld {
+    // Statements
+//}
+
+
+// public static void main(String[] args)  {...} is the main method
+// Every application in Java must contain the main method. The program execution starts at the main method
+// main method is mandatory
+
+//public static void main(String[] args) {
+//    .....
+//}
+
+// System.out.println("Hello, World"); is print to screen function.
+
+// PUNCH LINES
+
+/* This is a comment */
+// This is a comment
+// Every Java application must have a class definition that matches the filename
+// Every program start with the main method...this is not an option
+// The program execution starts from the main function
